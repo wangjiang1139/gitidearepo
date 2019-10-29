3e61dfe24b00a5d3d1acb10a6fddba5263344a94
wangjiang <541478324@qq.com>
2019-10-29 22:39:37 +0800

    创建一个类

---
+++ src/com/cn/cmbc/git/Demo0001.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: Demo0001
+ * Author:   wangjiang
+ * Date:     2019/10/29 22:37
+ * Description: 创建一个类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.cn.cmbc.git;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈创建一个类〉
+ *
+ * @author wangjiang
+ * @create 2019/10/29
+ * @since 1.0.0
+ */
+public class Demo0001 {
+    public static void main(String[] args) {
+        System.out.println("你好");
+    }
+}
