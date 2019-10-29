d8b399812917acfd00f7c73945cd24f31c5c4a05
wangjiang <541478324@qq.com>
2019-10-29 22:49:33 +0800

    再次修改这个类

--- src/com/cn/cmbc/git/Demo0001.java
+++ src/com/cn/cmbc/git/Demo0001.java
@@ -25,5 +25,8 @@ public class Demo0001 {
         System.out.println("你好");
         System.out.println("你好");
         System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
+        System.out.println("你好");
     }
 }
